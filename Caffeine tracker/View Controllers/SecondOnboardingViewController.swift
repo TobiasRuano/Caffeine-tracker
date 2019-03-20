93edd33103d0da0b91fa803baa4595b9c745ebf2
Tobias <ruano.t10@gmail.com>
2019-03-20 17:40:54 -0300

    Log water switch added to onboarding screen

--- Caffeine tracker/View Controllers/SecondOnboardingViewController.swift
+++ Caffeine tracker/View Controllers/SecondOnboardingViewController.swift
@@ -10,8 +10,29 @@ import UIKit
 
 class SecondOnboardingViewController: UIViewController {
 
+    @IBOutlet weak var logWaterView: UIView!
+    @IBOutlet weak var waterSwitch: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        styleLogWaterView()
+    }
+    
+    func styleLogWaterView() {
+        logWaterView.layer.cornerRadius = 10
+        logWaterView.layer.shadowRadius = 10
+        logWaterView.layer.shadowColor = UIColor.lightGray.cgColor
+        logWaterView.layer.masksToBounds = false
+        logWaterView.layer.shadowOpacity = 0.5
+    }
+    
+    @IBAction func switchAction(_ sender: UISwitch) {
+        if waterSwitch.isOn == true {
+            UserDefaults.standard.set(true, forKey: logWaterBoolKey)
+        }else {
+            UserDefaults.standard.set(false, forKey: logWaterBoolKey)
+        }
     }
     
     @IBAction func ExitToRootViewController(_ sender: UIButton) {

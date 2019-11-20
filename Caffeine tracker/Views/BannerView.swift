b0ec108d334dc2612a122cec2e14d2ae84febe54
Tobias Ruano <ruano.t10@gmail.com>
2019-11-20 18:54:12 -0300

    Primera implementacion del nuevo banner

---
+++ Caffeine tracker/Views/BannerView.swift
@@ -0,0 +1,26 @@
+//
+//  NotificationBanner.swift
+//  Caffeine tracker
+//
+//  Created by Tobias Ruano on 17/11/2019.
+//  Copyright © 2019 Tobias Ruano. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class BannerView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    func setupView() {
+        
+    }
+}

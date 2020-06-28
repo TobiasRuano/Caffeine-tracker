ac201528fe3b80c1c21eed1833a1dfef263d0249
Tobias Ruano <ruano.t10@gmail.com>
2020-06-28 11:48:18 -0300

    Create CoffeeFacts.swift

---
+++ Caffeine tracker/CustomClasses/CoffeeFacts.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeFacts.swift
+//  Caffeine tracker
+//
+//  Created by Tobias Ruano on 12/06/2020.
+//  Copyright © 2020 Tobias Ruano. All rights reserved.
+//
+
+import Foundation
+
+//class coffeFact {
+//    var description: String
+//
+//    func getFact() {
+//
+//    }
+//}
